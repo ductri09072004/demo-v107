5004093008ffc585f9e50e7db297fad9329b8ea6
ductri09072004 <145476276+ductri09072004@users.noreply.github.com>
2025-10-18 21:17:36 +0700

    Update mock_data.py

--- data/mock_data.py
+++ data/mock_data.py
@@ -9,7 +9,7 @@ def get_system_info():
     """Trả về thông tin hệ thống giả"""
     return {
         "system_info": {
-            "name": "Demo FISS API v107.13",
+            "name": "Demo FISS API v107.14",
             "version": "1.0.0",
             "environment": "development"
         },
